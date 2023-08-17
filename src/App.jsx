d3bb260a3ed8c53b1816df75a36525770940ae81
Fernando Monterroza <fernandomonterroza48@gmail.com>
2023-08-17 11:32:16 -0500

    hola test2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
+
+
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
